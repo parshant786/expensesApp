f3c63521faf471cd1a5a21a992186eb3859fdd55
Parshant Jain <parshant.jain@letx.co>
2024-06-03 16:30:24 +0530

    add changes

--- Screens/ManageExpenses/index.js
+++ Screens/ManageExpenses/index.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from "../../components/ui/Button";
 import { IconButton } from "../../components/ui/IconButton";
+import { GlobalStyles } from "../../constants/globalStyles";
 import { ExpensesContext } from "../../store/ExpensesContext";
 
 export const ManageExpensesScreen = ({ route, navigation }) => {
@@ -8,7 +10,6 @@ export const ManageExpensesScreen = ({ route, navigation }) => {
 
   let editing = !!route.params?.id;
   const handleDelete = () => {
-   
     handleDeleteExpense(route.params?.id);
     navigation.goBack();
   };
@@ -16,7 +17,39 @@ export const ManageExpensesScreen = ({ route, navigation }) => {
   return (
     <View>
       <Text>hi</Text>
-      <IconButton name={"remove-circle"} onPress={handleDelete} />
+
+      <View style={styles.buttonRowContainer}>
+        <View style={styles.buttonContainer}>
+          <Button text="cancel" mode="flat" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button text={editing ? "Update" : "Add"} />
+        </View>
+      </View>
+      <View style={styles.deleteButtonContainer}>
+        <IconButton
+          name={"trash"}
+          color={GlobalStyles.colors.error500}
+          onPress={handleDelete}
+        />
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  buttonRowContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    paddingHorizontal: 4,
+  },
+  deleteButtonContainer:{
+    borderTopWidth:1,
+    borderTopColor: "#cfcccc",
+    alignItems: "center",
+    paddingTop: 5,
+    marginTop:10
+  }
+});
---
+++ components/ui/Button.js
@@ -0,0 +1,40 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/globalStyles";
+
+export const Button = ({ text, onPress, mode }) => {
+  return (
+    <View>
+      <Pressable
+        style={({ pressed }) => pressed && styles.pressed}
+        onPress={onPress}
+      >
+        <View style={[styles.textContainer ,mode ==="flat" && styles.flatTextContainer]}>
+          <Text style={[styles.textStyle,mode ==="flat" && styles.flatTextStyle]}>{text}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  buttonContainer: {},
+  textContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 6,
+    backgroundColor: GlobalStyles.colors.accent500,
+  },
+  textStyle: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.5,
+    backgroundColor: GlobalStyles.colors.primary50,
+  },
+  flatTextContainer: {
+    backgroundColor: "transparent",
+  },
+  flatTextStyle: {
+    color: GlobalStyles.colors.accent500,
+  },
+});
